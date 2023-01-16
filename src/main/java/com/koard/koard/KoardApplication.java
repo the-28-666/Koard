8d681ca9f5a4a189e04684c00dcf525c2d0386c5
Jung-Kwon Lee <akdirdl@gmail.com>
2023-01-16 22:38:35 +0900

    init: 프로젝트 생성
    2023. 01. 16. 프로젝트 시작

---
+++ src/main/java/com/koard/koard/KoardApplication.java
@@ -0,0 +1,13 @@
+package com.koard.koard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koard/koard/KoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koard.koard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
